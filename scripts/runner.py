9f334094abd9714f98e0d0b785fb1753f4a33fb9
zimmer <zimmer@fysik.su.se>
2017-01-11 13:00:15 +0100

    minor fixes.

--- scripts/runner.py
+++ scripts/runner.py
@@ -1,12 +1,12 @@
 #!/usr/local/bin/python2.7
 # encoding: utf-8
-'''
+"""
 scripts.runner -- main script to execute DampeTaskRunner
 @author:     S. Zimmer
 @change:     2017-01-09:    initial import
              2017-01-11:    first complete implementation
 @fixme:      force does nothing so far.
-'''
+"""
 
 import sys
 from os import remove
@@ -18,8 +18,9 @@ from psutil import Process as PsProcess
 import logging
 from base.logger import initLogger
 
+
 def main(argv=None):
-    def run(cfg, log):
+    def run(cfg, log, pidfile):
         reco = RecoRunner(config=cfg)
         proc = Process(target=reco.execute)
         proc.start()
@@ -28,7 +29,12 @@ def main(argv=None):
         prm = ProcessResourceMonitor(ps)
         while proc.is_alive():
             log.info(prm.queryResources())
-            sleep(300.)  # check every 5 minutes
+            if not isfile(pidfile):
+                log.warning("PID file removed; requested shutdown.")
+                proc.terminate()
+            else:
+                sleep(600.)  # check every 5 minutes
+
         log.info("Execution complete")
 
     parser = ArgumentParser(description="main script to execute DAMPE Task Runner")
@@ -55,14 +61,13 @@ def main(argv=None):
     log = logging.getLogger("core")
 
     if not args.daemon:
-        run(args.cfg, log)
+        run(args.cfg, log, pidfile)
         remove(pidfile)
     else:
         log.info("running in daemon mode, will only terminate if pid file is removed")
         while isfile(pidfile):
-            run(args.cfg,log)
+            run(args.cfg,log, pidfile)
         log.info("PID file removed, shutting down daemon.")
 
 if __name__ == "__main__":
     sys.exit(main())
-    

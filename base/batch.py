733061daf064707df8e9c48d1a333409ab2fc7f7
zimmer <zimmer@fysik.su.se>
2017-02-02 02:46:11 +0100

    fixes.

--- base/batch.py
+++ base/batch.py
@@ -106,9 +106,11 @@ class slurm(hpc):
             lines = lines[1:-1]
             for line in lines[1:-1]:
                 while "\n" in line: line = line.replace("\n","")
-                my_line = line.split(" ")
+                my_line = [f for f in line.split(" ") if not f == ""]
                 log.error(str(my_line))
-                jobId, status = int(my_line[0]),my_line[3]
+                jobId = my_line[0]
+                if "_" in jobId: jobId = int(jobId.split("_")[0])
+                status = my_line[4]
                 while " " in status: status = status.replace(" ","")
                 jobs[jobId] = status
         return jobs
